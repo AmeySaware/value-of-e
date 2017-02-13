53d9b3d1783b836c304bb15cb231f8ba7ae55610
AmeySaware <saware.amey@gmail.com>
2017-02-13 17:39:15 +0100

    Version 1
    Code written using operators. Accuracy is really bad

--- e_compound _interest.py
+++ e_compound _interest.py
@@ -1,1 +1,16 @@
 #Created by Amey Saware
+
+"""
+Euler's constant represents natural growth over time. So value of unit capital with infinitismal interst rate and infinite time should be e.
+This program can be uses this fact to generate values of e. 
+"""
+
+def comp_int(period):
+
+	rate = float(1)/period
+
+	print (1+rate)**period
+
+	pass
+
+comp_int(10000000000000)
